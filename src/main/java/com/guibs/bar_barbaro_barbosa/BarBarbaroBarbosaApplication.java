2b8bc828842fd5f5ab1c8922d631b6302bf41992
GUILHERME BARBOSA SILVA <GBSILVA2@corporate.ad>
2026-02-13 16:51:23 -0300

    salvando projeto

---
+++ src/main/java/com/guibs/bar_barbaro_barbosa/BarBarbaroBarbosaApplication.java
@@ -0,0 +1,13 @@
+package com.guibs.bar_barbaro_barbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarBarbaroBarbosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarBarbaroBarbosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guibs/bar_barbaro_barbosa/BarBarbaroBarbosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guibs.bar_barbaro_barbosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarBarbaroBarbosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
